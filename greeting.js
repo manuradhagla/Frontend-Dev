ceec05eb7b164af2d5aff6ec301dd7bfbbc58bb6
Manuradha <your_email@example.com>
2025-11-26 11:30:21 +0530

    JS function problems solution

---
+++ greeting.js
@@ -0,0 +1,11 @@
+function showEndMessage() {
+    console.log('Welcome to the course!')
+}
+
+function greetUser(name, callback) {
+    console.log(`Hello ${name}`)
+    if (typeof callback === 'function') callback()
+}
+
+greetUser('Manuradha Singh', showEndMessage)
+
